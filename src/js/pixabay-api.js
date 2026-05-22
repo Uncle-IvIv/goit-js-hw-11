89733b4134e4141f246589951161953a0c478f73
Ivan <si-go@ukr.net>
2026-05-22 15:48:23 +0300

    fix: add api path to axios base url

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '55975550-00d0c24910787aa60712254b7'; 
-const BASE_URL = 'https://pixabay.com';
+const BASE_URL = 'https://pixabay.com/api/'; 
 
 export async function getImagesByQuery(query) {
   const response = await axios.get(BASE_URL, {
