d29d2b2016ebae95f9cc57946d383d5e48343113
Ivan <si-go@ukr.net>
2026-05-29 10:44:16 +0300

    feat: refactor with async-await and implement pagination for hw-12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,16 +3,17 @@ import axios from 'axios';
 const API_KEY = '55975550-00d0c24910787aa60712254b7'; 
 const BASE_URL = 'https://pixabay.com/api/'; 
 
-export async function getImagesByQuery(query) {
-  const response = await axios.get(BASE_URL, {
-    params: {
-      key: API_KEY,
-      q: query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-    },
-  });
-  
+export async function fetchImages(query, page = 1) {
+  const searchParams = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: page,  
+    per_page: 15, 
+  };
+
+  const response = await axios.get(BASE_URL, { params: searchParams });
   return response.data;
 }
